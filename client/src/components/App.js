a339d996925fb8db3f35292d02d145a551cde555
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-10-05 15:42:46 -0600

    Part 1 - steps 1-3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import NoMatch from './NoMatch';
 import NavBar from './NavBar';
 import Flash from './Flash';
 import Home from './Home';
+import Beers from './Beers';
 import { Switch, Route } from 'react-router-dom';
 import { Segment } from 'semantic-ui-react';
 
